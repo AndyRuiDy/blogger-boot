f7a90acc57ac59d95d9f3f4243af81fcedd2c349
zhush <andyjavadream@163.com>
2019-06-01 14:26:39 +0800

    first commit

---
+++ src/main/java/com/zhush/bloggerboot/BloggerBootApplication.java
@@ -0,0 +1,13 @@
+package com.zhush.bloggerboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggerBootApplication.class, args);
+    }
+
+}
